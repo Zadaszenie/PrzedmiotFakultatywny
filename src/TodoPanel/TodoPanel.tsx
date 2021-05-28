4412db7136d65b98a877e407de522dd16d5caff8
Zadaszenie <80452195+Zadaszenie@users.noreply.github.com>
2021-05-28 17:15:51 +0200

    Add files via upload

---
+++ src/TodoPanel/TodoPanel.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useAction } from '../../hooks/useAction';
+import { TodoService } from '../../servies/todo.service';
+import {Todo} from '../../store/reducers/todos.reducer';
+import todoSelector from '../../store/selectors/todo.selector';
+
+const TodoPanel = () => {
+
+    const todoActions = useAction(TodoService)
+    const todos = useSelector(todoSelector.getAll);
+
+    React.useEffect(() => {
+        
+        todoActions.setNewTodo(
+            {
+                id: 123,
+                label: 'testowyLabel',
+                done: false,
+                description: 'pierwszy wpis',
+            }
+        );
+    }, []);
+
+
+    const deleteTodo = (todo: Todo)=>
+    {
+        todoActions.setDeleteTodo(todo);
+    }
+    return (
+        <div>
+            {todos.map((todo, index) => (
+                <div key={index}>
+                    <button onClick={() => deleteTodo(todo)}> Remove</button>
+                    <p>{todo.label}</p>
+                    <p>{todo.done}</p>
+                    <p>{todo.description}</p>
+                </div>
+            ))}
+            Page not found!
+        </div>
+    );
+}
+
+export default TodoPanel;
